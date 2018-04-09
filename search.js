212dfd44cc15f669aea2a53862e40ed78a99d320
Niv Sardi <xaiki@evilgiggle.com>
2018-04-09 17:39:05 -0300

    hack around algorithm not working well with spaces
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- search.js
+++ search.js
@@ -20,22 +20,24 @@ class fuzzySearch {
         debug('new stringMap', this.opts)
         let stringMap = new StringMap(this.opts)
         this.items = items
-        Object.keys(items).map(k => stringMap.add(k, k))
+        Object.keys(items).map(k => stringMap.add(k.replace(/\s+/g, '_'), k.replace(/\s+/g, '_')))
         return stringMap
     }
 
     search(term, data) {
+        term = term.replace(/\s+/g, '_')
         let res = this.stringMap.search(term)
         if (!res) {
             return Promise.reject(new Error('not found'))
         }
 
-        debug('search', res)
         if (res[0].distance > this.opts.maxDistance) {
             return Promise.reject(new Error('found with bad distance'))
         }
 
-        return Promise.resolve(this.items[res[0].value])
+        let k = res[0].value.replace(/_/g, ' ')
+        debug('search', term, res, this.items[k])
+        return Promise.resolve(this.items[k])
                       .then(this.resolve.bind(this, data))
     }
 }
